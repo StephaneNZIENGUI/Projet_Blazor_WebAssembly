c7949d6901f6d14b0026f66721ea364733c5b50c
François NGOYE NZIENGUI <stephanengoye8@gmail.com>
2024-08-24 06:27:17 +0200

    Ajouter une base de donnée

---
+++ StudentsManagement/StudentsManagement/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsManagement.Models
+{
+    public class Student
+    {
+
+        [Key]
+        public int Id {  get; set; }
+        public required string FirstName {  get; set; }
+        public required string MiddleName {  get; set; }
+        public required string LastName {  get; set; }
+        public required string EmailAddress {  get; set; }
+        public required string Adress {  get; set; }
+        public required string PhoneNumber {  get; set; }
+        public required string Country {  get; set; }
+    }
+}
