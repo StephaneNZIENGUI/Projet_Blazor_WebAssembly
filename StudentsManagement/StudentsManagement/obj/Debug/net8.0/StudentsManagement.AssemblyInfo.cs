80e931e91e9d1f8d8a46d15ae1b4a6562720363d
François NGOYE NZIENGUI <stephanengoye8@gmail.com>
2024-08-26 06:09:13 +0200

    ajout Nuget packages, ajout repo et deplacement de model dans repo, ajout service

--- StudentsManagement/StudentsManagement/obj/Debug/net8.0/StudentsManagement.AssemblyInfo.cs
+++ StudentsManagement/StudentsManagement/obj/Debug/net8.0/StudentsManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1573be81c9471e8e31e9b0bc365275081655b86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7949d6901f6d14b0026f66721ea364733c5b50c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentsManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
